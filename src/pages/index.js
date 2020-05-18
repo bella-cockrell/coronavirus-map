f4fd86f54ae3e8744c3b83dc7f40bf0b79d6db19
bella-cockrell <heybellac@gmail.com>
2020-05-18 22:01:03 +0100

    Adding try/catch API request

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import L from 'leaflet';
+import axios from 'axios';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
@@ -15,14 +16,17 @@ const DEFAULT_ZOOM = 2;
 
 const IndexPage = () => {
 
-  /**
-   * mapEffect
-   * @description Fires a callback once the page renders
-   * @example Here this is and example of being used to zoom in and set a popup on load
-   */
-
   async function mapEffect({ leafletElement: map } = {}) {
+    let response;
 
+    try {
+      response = await axios.get('https://corona.lmao.ninja/v2/countries');
+    } catch (e) {
+      console.log(`Failed to fetch countries ${e.message}`, e);
+      return;
+    }
+
+    const { data = [] } = response;
   }
 
   const mapSettings = {
