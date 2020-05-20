f1cd9a0d1a5645401afefed63980a2528d8d3848
bella-cockrell <heybellac@gmail.com>
2020-05-20 15:13:06 +0100

    Adding new GeoJSON instance, function and string block for markers

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
+import { LeafletProvider } from 'react-leaflet';
 
 const LOCATION = {
   lat: 38.9072,
@@ -48,6 +49,56 @@ const IndexPage = () => {
         }
       })
     }
+    //new instance of L.GeoJSON for Leaflet to understand
+    //plus function to destructure and format data
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let caseString;
+
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered
+        } = properties
+
+        caseString = `${cases}`;
+
+        if (cases > 1000) {
+          caseString = `${caseString.slice(0, -3)}k+`
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+        //String block for map marker and tooltip
+        const html = `
+        <span class='icon-marker'>
+          <span class='icon-marker-tooltip'>
+            <h2>${country}</h2>
+            <ul>
+              <li><strong>Confirmed: </strong>${cases}</li>
+              <li><strong>Deaths: </strong>${deaths}</li>
+              <li><strong>Recovered: </strong>${recovered}</li>
+              <li><strong>Last update: </strong>${updatedFormatted}</li>
+            </ul>
+          </span>
+          ${caseString}
+        </span>
+      `;
+
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+    });
   }
 
   const mapSettings = {
